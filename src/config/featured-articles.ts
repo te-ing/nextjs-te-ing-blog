34a556c406fbdef5d1d1ee81558469a2d61fc686
te-ing <hi2177@naver.com>
2026-05-03 14:43:09 +0900

    post: Featured articles 목록 수정

--- src/config/featured-articles.ts
+++ src/config/featured-articles.ts
@@ -3,6 +3,6 @@
 export const featuredArticles = [
   '2025-05-23-Fiber-아키텍처',
   '2025-04-29-HTTP-캐시전략',
-  '2024-10-23-개발자로 취업한 지 2년 즈음이 지난 시점에서 적는 회고',
+  '2026-02-10-How-Claude-Code-Works',
   // 추가하고 싶은 글의 ID를 여기에 추가
 ];
